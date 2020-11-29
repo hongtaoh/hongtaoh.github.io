a585833e2162f674b80d8b83fd32414df9e40656
hongtaoh <fhaohongtao@163.com>
2020-11-29 02:40:43 -0500

    Rebuilding site Sun Nov 29 02:40:37 EST 2020

---
+++ public/js/center-image.js
@@ -0,0 +1,23 @@
+(function() {
+  function center_el(tagName) {
+    var tags = document.getElementsByTagName(tagName), i, tag;
+    for (i = 0; i < tags.length; i++) {
+      tag = tags[i];
+      var parent = tag.parentElement;
+      // center an image if it is the only element of its parent
+      if (parent.childNodes.length === 1) {
+        // if there is a link on image, check grandparent
+        if (parent.nodeName === 'A') {
+          parent = parent.parentElement;
+          if (parent.childNodes.length != 1) continue;
+          parent.firstChild.style.border = 'none';
+        }
+        if (parent.nodeName === 'P') parent.style.textAlign = 'center';
+      }
+    }
+  }
+  var tagNames = ['img', 'embed', 'object'];
+  for (var i = 0; i < tagNames.length; i++) {
+    center_el(tagNames[i]);
+  }
+})();
