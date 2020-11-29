b0e0839dec91c4a1deba2b7c1968850c46330776
hongtaoh <fhaohongtao@163.com>
2020-11-28 23:46:36 -0500

    Publishing to gh-pages (publish.sh)

--- deploy.sh
+++ deploy.sh
@@ -9,12 +9,6 @@ msg() {
 
 #!/bin/sh
 
-if [ "`git status -s`" ]
-then
-    echo "The working directory is dirty. Please commit any pending changes."
-    exit 1;
-fi
-
 echo "Deleting old publication"
 rm -rf public
 mkdir public
