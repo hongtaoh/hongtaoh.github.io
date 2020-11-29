f886a22e1f1fc9a81dc865dc06d879f74354c253
hongtaoh <fhaohongtao@163.com>
2020-11-28 23:52:48 -0500

    Updating site Sat Nov 28 23:52:48 EST 2020

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,16 @@
 export LC_TIME=en_US.UTF-8
 
 # The commit message.
-MESSAGE="Site rebuild $(date)"
+MESSAGE="Updating site $(date)"
+SOURCE=sources
 
 msg() {
     printf "\033[1;32m :: %s\n\033[0m" "$1"
 }
+
+git add .
+git commit -m "$MESSAGE"
+git push origin "$SOURCE"
 
 #!/bin/sh
 
@@ -16,7 +21,7 @@ git worktree prune
 rm -rf .git/worktrees/public/
 
 echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public upstream/gh-pages
+git worktree add -B gh-pages public origin/gh-pages
 
 echo "Removing existing files"
 rm -rf public/*
@@ -28,4 +33,4 @@ echo "Updating gh-pages branch"
 cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
 
 echo "Pushing to github"
-git push --all
+git push origin gh-pages
