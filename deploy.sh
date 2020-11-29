6a4a1f619c1b01fb410884162cab63faeeb484d8
hongtaoh <fhaohongtao@163.com>
2020-11-29 11:17:54 -0500

    Rebuilding site Sun Nov 29 11:17:54 EST 2020

--- deploy.sh
+++ deploy.sh
@@ -34,4 +34,5 @@ echo "Updating gh-pages branch"
 cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
 
 echo "Pushing to github"
-git push -all
+git push origin gh-pages
+git push origin "$SOURCE"
